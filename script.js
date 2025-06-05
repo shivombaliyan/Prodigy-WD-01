0caab4181d68798d66a27aa774cd03552d92a7b8
shivombaliyan <shivombaliyan999@gmail.com>
2025-06-05 11:52:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
+  
